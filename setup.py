3d70bbbcb41afd9547a7463ad532159ab4f4233b
cleonard <ccl@chrisleonard.com>
2021-07-12 18:42:31 -0500

    Cleaning up

--- setup.py
+++ setup.py
@@ -12,12 +12,10 @@ setup(
     author="Chris Leonard (@cleonard)",
     author_email="ccl@chrisleonard.com",
     description="A demonstration of the Python Click package",
-    py_modules=["weather_click"],
     entry_points="""
         [console_scripts]
         get_weather=weather_api:cli
      """,
-    include_package_data=True,
     install_requires=install_requires,
     version="0.0.1",
     url="https://github.com/cleonard/click_pkg_demo_atx_python",
