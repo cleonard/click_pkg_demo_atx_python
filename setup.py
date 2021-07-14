b52273375b283fc10ef956cabdf3fdae49d22f44
cleonard <ccl@chrisleonard.com>
2021-07-14 14:17:15 -0500

    Moved provisioning database to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import sqlite3
 import sys
 
 from setuptools import setup
@@ -5,7 +7,28 @@ from setuptools import setup
 if sys.version_info < (3, 6, 0):
     sys.exit("Python 3.6+ required but lower version found. Aborted.")
 
-install_requires = ["click", "requests"]
+"""Creates a sqlite database for storing weather data"""
+
+DB_FILE = "weather.db"
+
+cnx = sqlite3.connect(DB_FILE)
+cursor = cnx.cursor()
+
+create_table = """
+create table weather
+(id INTEGER PRIMARY KEY,
+city TEXT,
+county_code TEXT,
+timestamp INTEGER,
+temp REAL,
+description TEXT);
+"""
+
+cursor.execute(create_table)
+cursor.close()
+cnx.close()
+
+requirements = ["click", "requests"]
 
 setup(
     name="click_pkg_demo_atx_python",
@@ -16,7 +39,7 @@ setup(
         [console_scripts]
         get_weather=weather_service.service:cli
      """,
-    install_requires=install_requires,
+    install_requires=requirements,
     version="0.0.1",
     url="https://github.com/cleonard/click_pkg_demo_atx_python",
 )
